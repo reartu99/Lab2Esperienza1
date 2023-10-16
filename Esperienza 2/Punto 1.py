2266ba3e0931b7f8904577d790685cf1c41b5c6a
Arturo <arturo.masullo2003@icloud.com>
2023-10-16 23:52:57 +0200

    Aggiustato errore esperienza 2

--- Esperienza 2/Punto 1.py
+++ Esperienza 2/Punto 1.py
@@ -13,6 +13,7 @@ def line(x, a, b):
     return a * np.array(x) + b
 
 
+Ev0 = 0 # valore errore del generatore
 Amperaggio = [7, 8, 9, 10, 11, 12, 14, 17, 21, 28, 29, 30, 31, 32, 33, 34, 36, 37, 39, 40]  # milliamp
 Amperaggio = np.array(Amperaggio)*0.001
 Voltaggio = [8.21, 8.05, 7.86, 7.64, 7.36, 7.0, 6.52, 5.87, 4.91, 3.363, 3.145, 2.92, 2.679, 2.422, 2.146, 1.85, 1.53,
@@ -25,24 +26,35 @@ EAmp = 0.6
 
 popt, pcov = sp.optimize.curve_fit(line, Amperaggio, Voltaggio)
 plt.errorbar(Amperaggio, Voltaggio, EAmp, ErrVolt, fmt="o", markersize=4)
-print(popt, pcov)
+print("I valori del fit per il primo punto sono: " + str(popt))
+print("La matrice di covarianza per il primo punto è: " + str(pcov))
+print("")
 plt.plot(Amperaggio, line(Amperaggio, popt[0], popt[1]))
 plt.show()
 
-# Con il valore misurato della resistenza
-R01 = 215.4  # Ohm
+# I valori predetti dal teorema di thevenin per il primo punto sono:
 Vt1 = 10  # Volt
+# 0) tdc con il valore nominale del generatore
+TDC00 = moduloanalisi.tdc(Vt1, popt[1], 0, pcov[0][0])
+print("Il valore dato dal tdc con il valore nominale del voltaggio è: " + str(TDC00))
+
+# Poi le resistenze sono dispiegate nei seguenti casi:
+
+# 1) tdc con il valore misurato della resistenza
+R01 = 215.4  # Ohm
+
 ER01 = moduloanalisi.incertezzadigitale(1.2, R01, 2, 0.1)
 
 TDC11 = moduloanalisi.tdc(R01, -1*popt[0], ER01, pcov[1][1])
-print(TDC11)
+print("Il valore dato dal tdc con il valore misurato della resistenza è: " + str(TDC11))
 
-# Con il valore nominale della resistenza
-R01 = 220  # Ohm
-ER01 = 0.1*R01/np.sqrt(3)
+# 2) tdc con il valore nominale della resistenza
+R011 = 220  # Ohm
+ER011 = 0.1*R011/np.sqrt(3)
 
-TDC22 = moduloanalisi.tdc(R01, -1*popt[0], ER01, pcov[1][1])
-print(TDC22)
+TDC22 = moduloanalisi.tdc(R011, -1*popt[0], ER011, pcov[1][1])
+print("Il valore dato dal tdc con il valore nominale della resistenza è: " + str(TDC22))
+
 
 # Seconda parte primo punto
 Amperaggio2 = [23, 23, 24, 25, 26, 27, 28, 29, 31, 32, 34, 35, 37, 39, 42, 44, 47, 50]
@@ -54,11 +66,24 @@ ErrVolt2 = mm.incertezzadigitale(1, np.array(Voltaggio2), 3, 0.001)
 # Ogni tacca quindi vale 1mA e quindi divisa rad(3) viene 0.577
 popt2, pcov2 = sp.optimize.curve_fit(line, Amperaggio2, Voltaggio2)
 plt.errorbar(Amperaggio2, Voltaggio2, EAmp, ErrVolt2, fmt="o", markersize=4)
-print(popt2, pcov2)
+print("")
+print("I valori del fit per il secondo punto sono: " + str(popt2))
+print("La matrice di covarianza per il secondo punto è: " + str(pcov2))
+print("")
 plt.plot(Amperaggio2, line(Amperaggio2, popt2[0], popt2[1]))
 plt.show()
 
+# I valori predetti dal teorema di Thevenin per la seconda parte sono
+Er = 0.1*2200/np.sqrt(3)
 Vt = 1/2 * 15
-R0 = 220*220/440
+Evt = 1/2*Ev0
+R0 = 2200*2200/4400
+ER0 = np.sqrt(np.power(1100*Er, 2))
 
-print(Vt, R0)
+# TDC tra valore misurato e predetto da Thevenin
+TDC31 = moduloanalisi.tdc(popt2[0], pcov[0][0], Vt, Evt)
+print("Il valore dato dal tdc con il valore misurato e quello di Thevenin per il voltaggio è: " + str(TDC31))
+
+# TDC valore misurato con quello di Thevenin resistenza
+TDC32 = moduloanalisi.tdc(popt[1], R0, pcov[1][1], ER0)
+print("Il valore dato dal tdc con il valore misurato e quello di Thevenin per la resistenza è: " + str(TDC32))

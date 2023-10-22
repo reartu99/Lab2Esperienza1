c6606a4ea226cc0ff1276b4d9549d76ca310e291
Arturo <arturo.masullo2003@icloud.com>
2023-10-22 19:19:50 +0200

    Aggiustato errore esperienza 2

--- Esperienza 2/Punto 2.py
+++ Esperienza 2/Punto 2.py
@@ -5,11 +5,6 @@ import moduloanalisi as mm
 import numpy as np
 import scipy as sp
 
-
-def line(x, a, b):
-    return a * np.array(x) + b
-
-
 V0 = 16
 R1_valore = 100.3
 R2_valore = 2*R1_valore
@@ -20,8 +15,8 @@ ER2 = 2*ER1
 
 Alfa = (2 * R2_valore) / (2 * R2_valore + R1_valore + np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore))
 
-derivata_alfa_R1 = (2 * R2_valore * (1 - (2 * R1_valore + R2_valore) / np.sqrt(R1_valore*2 + 4 * R1_valore * R2_valore))) / (2 * R2_valore + R1_valore + np.sqrt(R1_valore2 + 4 * R1_valore * R2_valore))*2
-derivata_alfa_R2 = 2 / (2 * R2_valore + R1_valore + np.sqrt(R1_valore*2 + 4 * R1_valore * R2_valore)) - (4 * R2_valore) / (np.sqrt(R1_valore2 + 4 * R1_valore * R2_valore) * (2 * R2_valore + R1_valore + np.sqrt(R1_valore2 + 4 * R1_valore * R2_valore))*2)
+derivata_alfa_R1 = (2 * R2_valore * (1 - (2 * R1_valore + R2_valore) / np.sqrt(R1_valore*2 + 4 * R1_valore * R2_valore))) / (2 * R2_valore + R1_valore + np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore))*2
+derivata_alfa_R2 = 2 / (2 * R2_valore + R1_valore + np.sqrt(R1_valore*2 + 4 * R1_valore * R2_valore)) - (4 * R2_valore) / (np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore) * (2 * R2_valore + R1_valore + np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore))*2)
 
 Ealfa = np.sqrt((derivata_alfa_R1*ER1)*2+(derivata_alfa_R2*ER2)*2)
 EV0 = 0.01/np.sqrt(3)
@@ -30,6 +25,9 @@ Vn = [15.96, 7.92 , 3.96, 1.98, 0.99]
 Nstadi = [1,2,3,4,5]
 
 ErrVn = mm.incertezzadigitale(1, np.array(Vn), 3, 0.01)
+
+def line(x, a, b):
+    return a * np.array(x) + b
 
 
 # Crea il grafico a dispersione (scatter plot) dei dati
@@ -49,7 +47,7 @@ print(popt, pcov)
 plt.plot(Nstadi, line(Nstadi, popt[0], popt[1]))
 plt.show()
 
-TDC1 = moduloanalisi.tdc(popt[0], Alfa, pcov[0][0], Ealfa)
+TDC1 = moduloanalisi.tdc(popt[0],Alfa ,pcov[0][0], Ealfa)
 print(TDC1)
-TDC2 = moduloanalisi.tdc(popt[1], V0, pcov[1][1], EV0)
+TDC2 = moduloanalisi.tdc(popt[1],V0,pcov[1][1], EV0)
 print(TDC2)

bdd6a8406143e8b0a78f6c82e557b273b3e5b4f1
Arturo <arturo.masullo2003@icloud.com>
2023-10-22 19:25:17 +0200

    Aggiustato errore esperienza 2

--- Esperienza 2/Punto 2.py
+++ Esperienza 2/Punto 2.py
@@ -7,24 +7,30 @@ import scipy as sp
 
 V0 = 16
 R1_valore = 100.3
-R2_valore = 2*R1_valore
-DeltaY = []
+R2_valore = 2 * R1_valore
 
 ER1 = moduloanalisi.incertezzadigitale(1.2, R1_valore, 2, 0.1)
-ER2 = 2*ER1
+ER2 = 2 * ER1
 
-Alfa = (2 * R2_valore) / (2 * R2_valore + R1_valore + np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore))
+Alfa = (2 * R2_valore) / (2 * R2_valore + R1_valore + np.sqrt(R1_valore ** 2 + 4 * R1_valore * R2_valore))
 
-derivata_alfa_R1 = (2 * R2_valore * (1 - (2 * R1_valore + R2_valore) / np.sqrt(R1_valore*2 + 4 * R1_valore * R2_valore))) / (2 * R2_valore + R1_valore + np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore))*2
-derivata_alfa_R2 = 2 / (2 * R2_valore + R1_valore + np.sqrt(R1_valore*2 + 4 * R1_valore * R2_valore)) - (4 * R2_valore) / (np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore) * (2 * R2_valore + R1_valore + np.sqrt(R1_valore**2 + 4 * R1_valore * R2_valore))*2)
+derivata_alfa_R1 = ((2 * R2_valore * (1 - (2 * R1_valore + R2_valore) /
+                                      np.sqrt(R1_valore ** 2 + 4 * R1_valore * R2_valore))) /
+                    (2 * R2_valore + R1_valore + np.sqrt(R1_valore ** 2 + 4 * R1_valore * R2_valore)) ** 2)
 
-Ealfa = np.sqrt((derivata_alfa_R1*ER1)*2+(derivata_alfa_R2*ER2)*2)
-EV0 = 0.01/np.sqrt(3)
+derivata_alfa_R2 = (2 / (2 * R2_valore + R1_valore + np.sqrt(R1_valore ** 2 + 4 * R1_valore * R2_valore))
+                    - (4 * R2_valore) / (np.sqrt(R1_valore ** 2 + 4 * R1_valore * R2_valore) *
+                                         (2 * R2_valore + R1_valore + np.sqrt(
+                                             R1_valore ** 2 + 4 * R1_valore * R2_valore)) * 2))
 
-Vn = [15.96, 7.92 , 3.96, 1.98, 0.99]
-Nstadi = [1,2,3,4,5]
+Ealfa = np.sqrt((derivata_alfa_R1 * ER1) * 2 + (derivata_alfa_R2 * ER2) * 2)
+EV0 = 0.01 / np.sqrt(3)
+
+Vn = [15.96, 7.92, 3.96, 1.98, 0.99]
+Nstadi = [1, 2, 3, 4, 5]
 
 ErrVn = mm.incertezzadigitale(1, np.array(Vn), 3, 0.01)
+
 
 def line(x, a, b):
     return a * np.array(x) + b
@@ -36,10 +42,8 @@ plt.errorbar(Nstadi, Vn, ErrVn, 0, fmt="o", markersize=4)
 # Mostra il grafico
 plt.show()
 
-
 log_Vn = np.log(Vn)
-DeltaY = ErrVn/Vn
-
+DeltaY = ErrVn / Vn
 
 popt, pcov = sp.optimize.curve_fit(line, Nstadi, log_Vn)
 plt.errorbar(Nstadi, log_Vn, DeltaY, 0, fmt="o", markersize=4)
@@ -47,7 +51,7 @@ print(popt, pcov)
 plt.plot(Nstadi, line(Nstadi, popt[0], popt[1]))
 plt.show()
 
-TDC1 = moduloanalisi.tdc(popt[0],Alfa ,pcov[0][0], Ealfa)
+TDC1 = moduloanalisi.tdc(popt[0], Alfa, pcov[0][0], Ealfa)
 print(TDC1)
-TDC2 = moduloanalisi.tdc(popt[1],V0,pcov[1][1], EV0)
+TDC2 = moduloanalisi.tdc(popt[1], V0, pcov[1][1], EV0)
 print(TDC2)
